fe64f1b070eab136aa25478bd70bd35f5ce5b8a3
Aditya <adityaagupta31@gmail.com>
2020-11-01 22:44:25 +0530

    intalled dependencies and connected the database

---
+++ router/users.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+//endUser registration
+
+
+//Admin registration
+
+
+//root Admin registration
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+//Load config
+dotenv.config({ path: "./config/config.env" });
+//Connecting to DATABASE
+connectDB();
+const app = express();
+
+//middleWares
+app.use(cors());
+app.use(bodyParser.json());
+
+//User router middleWare
+app.use("/api/users", require("./router/users"));
+
+//Connecting the APP
+const PORT = process.env.PORT || 3000;
+app.listen(
+  PORT,
+  console.log(`Server Listening in ${process.env.NODE_ENV} on port ${PORT}`)
+);
